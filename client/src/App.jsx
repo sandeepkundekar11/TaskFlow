e07305aa38c2d9b2cc6e351e76c89a28544f50c6
Sandeep K <sandeep@priyarajaelectronics.com>
2025-01-01 22:52:12 +0530

    upadted the Signuphook which handler the Signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+import React, { Suspense } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+const AddMinistratorSignup = React.lazy(() => import("./Components/MainComponents/AdministratorSignUp"))
+const AdministartorLogin = React.lazy(() => import("./Components/MainComponents/AdministratorLogin"))
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Suspense fallback={<h1>Loading....</h1>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AddMinistratorSignup />} />
+          <Route path="/administrativeLogin" element={<AdministartorLogin />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   )
 }
 
